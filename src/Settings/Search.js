4d4af52045b5cbd1575be4fb6c165db08d879d26
John Cawelti <caweltjr@hotmail.com>
2019-08-06 10:16:49 -0700

    Fetch Prices

--- src/Settings/Search.js
+++ src/Settings/Search.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import {backgroundColor2, fontSize2} from "../Shared/Styles";
+import {AppContext} from "../App/AppProvider";
+import _ from 'lodash';
+import fuzzy from 'fuzzy';
 
 const SearchGrid = styled.div`
     display: grid;
@@ -14,10 +17,38 @@ const SearchInput = styled.input`
     color: #1163c9;
     place-self: center left;
 `
+const handleFilter = _.debounce((inputValue, coinList, setFilteredCoins) => {
+    // get all the coin symbols
+    let coinSymbols = Object.keys(coinList);
+    // get all the coin names and map the symbol to the name
+    let coinNames = coinSymbols.map(sym => coinList[sym].CoinName);
+    // combine symbols and names to form string of all the strings to search
+    let allStringsToSearch = coinSymbols.concat(coinNames);
+    let fuzzyResults = fuzzy
+        .filter(inputValue, allStringsToSearch, {})
+        .map(result => result.string);
+    let filteredCoins = _.pickBy(coinList, (result,symKey) => {
+        let coinName = result.CoinName;
+        return (_.includes(fuzzyResults, symKey) || _.includes(fuzzyResults, coinName))
+    })
+    setFilteredCoins(filteredCoins);
+    }, 500);
 
+function filterCoins(e, setFilteredCoins, coinList){
+    let inputValue = e.target.value;
+    if(!inputValue){
+        setFilteredCoins(null);
+        return;
+    }
+    handleFilter(inputValue, coinList, setFilteredCoins);
+}
 export default function(){
-    return (<SearchGrid>
+    return (<AppContext.Consumer>
+        {({setFilteredCoins, coinList }) =>
+            <SearchGrid>
                 <h2>Search All Coins</h2>
-            <SearchInput/>
-            </SearchGrid>)
+                <SearchInput onKeyUp={(e) => filterCoins(e, setFilteredCoins, coinList)}/>
+            </SearchGrid>
+        }
+        </AppContext.Consumer>)
 }
